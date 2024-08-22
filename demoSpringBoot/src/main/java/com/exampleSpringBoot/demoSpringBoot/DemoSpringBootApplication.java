f45bc86907a0d020683140bd8ead4358e9e540a5
Sachin2442Bajpai <134322852+Sachin2442Bajpai@users.noreply.github.com>
2024-08-22 17:05:57 +0530

    Update DemoSpringBootApplication.java
    Add statement

--- demoSpringBoot/src/main/java/com/exampleSpringBoot/demoSpringBoot/DemoSpringBootApplication.java
+++ demoSpringBoot/src/main/java/com/exampleSpringBoot/demoSpringBoot/DemoSpringBootApplication.java
@@ -8,6 +8,7 @@ public class DemoSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBootApplication.class, args);
+		System.out.println("Hello Sachin Bajpai");
 	}
 
 }
